b6529bb174c6d63cabe3f1b55460f2ee2b58616b
yangcunfeng <yangcunfeng@51yche.com>
2017-07-19 17:06:26 +0800

    g

--- README.md
+++ README.md
@@ -12,3 +12,4 @@
 <a href="https://github.com/tiancityycf/algorithm/blob/master/echoprint.php" target="_blank" >echoprint优先级问题</a></br>
 <a href="https://github.com/tiancityycf/algorithm/blob/master/hashTable.php" target="_blank" >哈希表</a></br>
 <a href="https://github.com/tiancityycf/algorithm/blob/master/data.php" target="_blank" >链表、栈、队列模拟</a></br>
+<a href="https://github.com/tiancityycf/algorithm/blob/master/splFixedArray.php" target="_blank" >splFixedArray</a></br>
--- splFixedArray.php
+++ splFixedArray.php
@@ -1,5 +1,10 @@
 <?php
-    $len = 10;
+/**
+ * SplFixedArray主要是处理数组相关的主要功能，与普通php array不同的是，它是固定长度的，且以数字为键名的数组，优势就是比普通的数组处理更快。通常情况下SplFixedArray要比php array快上20%~30%，所以如果你是处理巨大数量的固定长度数组，还是强烈建议使用。
+ */
+splTest();
+function splTest(){
+    $len = 5;
     $array = new SplFixedArray($len);
     $arr = array();
     $arr["id"] = 1;
@@ -7,4 +12,47 @@
         $array[$i] = $arr; 
     }
     var_dump($array);
+
+    echo "\n获取数组长度=";
+    echo $array->getSize();
+    echo "=";
+    echo count($array);
+    echo "\n";
+    //设置数组长度
+    $array->setSize(6);
+    var_dump($array);
+}
+/**
+ * SplFixedArray性能分析
+ */
+splTime();
+function splTime(){
+    for ($size=1000; $size<=500000; $size*=2) {  
+        pr(PHP_EOL . "Testing size: $size" . PHP_EOL);   
+        for($s = microtime(true), $container = Array(), $i = 0; $i < $size; $i++) {  
+            $container[$i] = NULL;   
+        }  
+        pr( "Array(): " . (microtime(true) - $s) . PHP_EOL);   
+
+        for($s = microtime(true), $container = new SplFixedArray($size), $i = 0; $i < $size; $i++) {  
+            $container[$i] = NULL;   
+        }  
+        pr("SplArray(): " . (microtime(true) - $s) . PHP_EOL);   
+    }  
+}
+function pr() {  
+    $params = func_get_args();  
+    $env = php_sapi_name();  
+    if ("cli" == $env) {  
+        foreach ($params as $key => $value) {  
+            echo $value;  
+        }  
+    } else {  
+        foreach ($params as $key => $value) {  
+            echo "<pre>";  
+            print_r($value);  
+            echo "</pre>";  
+        }  
+    }  
+} // 用来打印输出结果  
 ?>
