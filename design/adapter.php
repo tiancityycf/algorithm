2986bc712df2e746c8025a6b668148d7af97602e
yangcunfeng <tiancityycf@163.com>
2019-06-15 15:08:21 +0800

    适配器模式

--- design/adapter.php
+++ design/adapter.php
@@ -76,3 +76,59 @@ class Test
 }
 $test = new Test();
 $test->run();
+
+/**
+ *   应用场景：老代码接口不适应新的接口需求，或者代码很多很乱不便于继续修改，或者使用第三方类库。例如：php连接数据库的方法：mysql,,mysqli,pdo,可以用适配器统一
+ */
+
+ //老的代码    
+
+ class User {   
+
+     private $name;   
+
+     function __construct($name) {   
+
+         $this->name = $name;   
+
+     }   
+
+     public function getName() {   
+
+         return $this->name;   
+
+     }   
+
+ }   
+ //新代码，开放平台标准接口   
+
+ interface UserInterface {   
+
+     function getUserName();   
+
+ }   
+
+ class UserInfo implements UserInterface {   
+
+     protected $user;   
+
+     function __construct($user) {   
+
+         $this->user = $user;   
+
+     }   
+
+     public function getUserName() {   
+
+         return $this->user->getName();   
+
+     }   
+
+ }   
+ $olduser = new User('张三');   
+
+ echo $olduser->getName();   
+
+ $newuser = new UserInfo($olduser);   
+
+ echo $newuser->getUserName(); 
